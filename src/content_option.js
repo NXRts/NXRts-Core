6fa39b89f7d91760cf325089d353034f1ce47188
NXRts <dangouu123@gmail.com>
2025-03-12 11:35:29 +0700

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,119 @@
+const logotext = "NXRts";
+const meta = {
+    title: "NXRts",
+    description: "I’m MYusuf Arrofi Full stack devloper,currently working in Karanganyar",
+};
+
+const introdata = {
+    title: "I’m MYusuf Arrofi",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop APi Websites and Apps",   
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: "https://images.unsplash.com/photo-1513789181297-6f2ec112c0bc",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [{
+        jobtitle: "Backend Enginner",
+        where: "Yusuf",
+        date: "2025",
+    },
+    {
+        jobtitle: "web developer",
+        where: "Yusuf",
+        date: "2024",
+    },
+    {
+        jobtitle: "web developer",
+        where: "Yusuf",
+        date: "2023",
+    },
+    {
+        jobtitle: "web developer",
+        where: "Yusuf",
+        date: "2022",
+    },
+];
+
+const skills = [{
+        name: "Golang",
+        value: 40
+    },
+    {
+        name: "Javascript",
+        value: 60,
+    },
+    {
+        name: "React",
+        value: 30,
+    },
+    {
+        name: "Next",
+        value: 35,
+    },
+    {
+        name: "Node Js",
+        value: 50,
+    },
+    {
+        name: "HTML CSS",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [
+    {
+        img: "https://images.unsplash.com/photo-1513789181297-6f2ec112c0bc",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1513789181297-6f2ec112c0bc",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
